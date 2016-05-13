e803faddaa910c218b397e1eb255c1713f24a4f4
Mara Behles Dean <maraebehles@gmail.com>
2016-05-13 10:56:36 -0600

    add some gems and .gitignore
    I also changed some code
    I also added pry

--- ruby_classes_modules_examples.rb
+++ ruby_classes_modules_examples.rb
@@ -1,14 +1,41 @@
+require 'pry'
+require 'babbler'
+
 puts 'YAY Friday!'
 
 def keep_coding
   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
   input = gets.strip.downcase
+
   if input == 'yes'
     puts 'Good answer!'
   else
-    puts 'You serious, bro? Do you even code?'
+    # puts 'You serious, bro? Do you even code?'
+    puts Babbler.babble
     keep_coding
   end
 end
 
 keep_coding
+
+
+# def keep_coding
+#   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
+#   input = gets.strip.downcase
+#   if input == 'quit'
+#   	exit
+#   else
+#   	input = input.to_i
+#   end
+# # this is the ternary code example. replaces above section that's commented out
+#   input == 'quit' ? exit : input = input.to_i
+
+#   if input == 'yes'
+#     puts 'Good answer!'
+#   else
+#     puts 'You serious, bro? Do you even code?'
+#     keep_coding
+#   end
+# end
+
+# keep_coding
